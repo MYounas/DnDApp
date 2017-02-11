02126999532c856488270d15b43623b538d1db2a
MYounas <younus.ubit56@gmail.com>
2017-02-11 19:22:19 +0500

    befor drag dro.js

--- DnDApp/Scripts/myApp.js
+++ DnDApp/Scripts/myApp.js
@@ -1,39 +1,70 @@
 ﻿
-var app = angular.module('myApp', ['dndLists']);
-app.controller('myController', ['$http', function ($http) {
-    var vm = this;
+//var app = angular.module('myApp', ['dndLists']);
+//app.controller('myController', ['$http', function ($http) {
+//    var vm = this;
 
-    vm.list1 = [];
-    vm.list2 = [];
-    vm.list3 = [];
+//    vm.list1 = [];
+//    vm.list2 = [];
+//    vm.list3 = [];
 
-    vm.getList1 = function () {
-        $http.get('/home/getList1').then(function (response) {
-            vm.list1 = response.data;
-        });
-    }
+//    vm.getList1 = function () {
+//        $http.get('/home/getList1').then(function (response) {
+//            vm.list1 = response.data;
+//        });
+//    }
 
-    vm.getList1();
+//    vm.getList1();
 
-    vm.falto=function(item){
-        alert(JSON.stringify(item));
-    }
+//    vm.falto=function(item){
+//        alert(JSON.stringify(item));
+//    }
 
-    vm.getList2 = function () {
-        $http.get('/home/getList2').then(function (response) {
-            vm.list2 = response.data;
-        });
-    }
+//    vm.getList2 = function () {
+//        $http.get('/home/getList2').then(function (response) {
+//            vm.list2 = response.data;
+//        });
+//    }
 
-    vm.getList2();
+//    vm.getList2();
 
-    vm.getList3 = function () {
-        $http.get('/home/getList3').then(function (response) {
-            vm.list3 = response.data;
-        });
-    }
+//    vm.getList3 = function () {
+//        $http.get('/home/getList3').then(function (response) {
+//            vm.list3 = response.data;
+//        });
+//    }
 
-    vm.getList3();
+//    vm.getList3();
 
 
-}]);
+//}]);
+
+var App = angular.module('drag-and-drop', ['ngDragDrop']);
+
+App.controller('oneCtrl', function ($scope, $timeout) {
+    $scope.list1 = [];
+    $scope.list2 = [];
+    $scope.list3 = [];
+    $scope.list4 = [];
+
+    $scope.list5 = [
+      { 'title': 'Item 1', 'drag': true },
+      { 'title': 'Item 2', 'drag': true },
+      { 'title': 'Item 3', 'drag': true },
+      { 'title': 'Item 4', 'drag': true },
+      { 'title': 'Item 5', 'drag': true },
+      { 'title': 'Item 6', 'drag': true },
+      { 'title': 'Item 7', 'drag': true },
+      { 'title': 'Item 8', 'drag': true }
+    ];
+
+    // Limit items to be dropped in list1
+    $scope.optionsList1 = {
+        accept: function (dragEl) {
+            if ($scope.list1.length >= 2) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+    };
+});
